6ab05aad6cd289e2fe017a369c3e29856de40eab
JuJuG3z <julien.gentili@gmail.com>
2025-02-24 18:48:25 +0100

    file src/types/ProductTypes.js created by Icarus.Ai

---
+++ src/types/ProductTypes.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export const ProductPropTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired
+};
