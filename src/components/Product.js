36a2a3c95a4bea217bdc20a14afcde4eb76a0d08
JuJuG3z <julien.gentili@gmail.com>
2025-02-24 18:48:28 +0100

    file src/components/Product.js updated by Icarus.AI

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,38 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import FormatPrice from '../Helpers/FormatPrice';
 import PropTypes from 'prop-types';
+import FormatPrice from '../Helpers/FormatPrice';
 
-const Product = (curElem) => {
-    const {id, name, image, price, category} = curElem;
-    
-    return (
-        <NavLink to={`/singleproduct/${id}`}>
-            <div className="card">
-                <figure>
-                    <img src={image} alt={name} />
-                    <figcaption className="caption">{category}</figcaption>
-                </figure>
+const Product = ({ id, name, image, price, category }) => {
+  return (
+    <NavLink to={`/singleproduct/${id}`}>
+      <div className="card">
+        <figure>
+          <img src={image} alt={name} />
+          <figcaption className="caption">{category}</figcaption>
+        </figure>
 
-                <div className="card-data">
-                    <div className="card-data-flex">
-                        <h3>{name || 'Unnamed Product'}</h3>
-                        <p className="card-data--price">
-                            <FormatPrice price={price}/>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </NavLink>
-    )
-}
-
-Product.propTypes = {
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    category: PropTypes.string.isRequired
+        <div className="card-data">
+          <div className="card-data-flex">
+            <h3>{name || 'Unnamed Product'}</h3>
+            <p className="card-data--price">
+              <FormatPrice price={price} />
+            </p>
+          </div>
+        </div>
+      </div>
+    </NavLink>
+  );
 };
 
-export default Product
+Product.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired
+};
+
+export default Product;
