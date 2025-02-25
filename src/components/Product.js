cab9ef78051f201fa70265eeb664db0263a6b864
JuJuG3z <julien.gentili@gmail.com>
2025-02-25 12:06:26 +0100

    file src/components/Product.js updated by Icarus.AI

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,13 +7,13 @@ const Product = (curElem) => {
     <NavLink to={`/singleproduct/${id}`}>
     <div className="card">
       <figure>
-        <img src={null} alt={name}  />
+        <img src={image} alt={name}  />
         <figcaption className="caption">{category}</figcaption>
       </figure>
 
       <div className="card-data">
         <div className="card-data-flex">
-          <h3>{"name"}</h3>
+          <h3>{name}</h3>
           <p className="card-data--price"><FormatPrice price={price}/></p>
         </div>
       </div>
